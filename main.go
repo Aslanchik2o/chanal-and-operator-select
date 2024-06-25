00c79712c6270b4817051cf1a8f7711441ef15b4
Aslan <admrobloks@gmail.com>
2024-06-25 10:59:00 +0500

    first comm

--- main.go
+++ main.go
@@ -2,24 +2,13 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
-	//cnanal принимает string\int\bool и тд
-	var msq chan int
-	fmt.Println(msq)
-	msq = make(chan int )
-	//каналы работают как указатель наверное:\
-	fmt.Println(msq)
-go func() {
-	time.Sleep(time.Second)
-	msq <- 12
-}()
-value :=<- msq
-fmt.Println(value)
-	//это типа мы записываем в канал 
-	// все это работает так же и с int хз насчет bool
-
+	msq := make(chan string, 4)//буферезованный канал
+	msq <- "Ниняно"
+	msq <- "Ниняно 2"
+fmt.Println(<-msq)
+fmt.Println(<-msq)
 	
 }
